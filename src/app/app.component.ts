2061f906e34e1f6942f234f9d59abb126811ff15
Ismael Asimbaya <ciasimbaya@uce.edu.ec>
2022-06-27 19:57:03 -0500

    proyecto angular 27/06/2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto_angular_ca';
+  title = 'Hola Mundo en Angular desde el TypeScript';
 }
